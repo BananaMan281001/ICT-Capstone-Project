22ed01a4448b5730a9762d60c6a614ba265e8c81
BananaMan281001 <ethanha051@gmail.com>
2022-09-29 18:35:12 +1000

    Adding home, assignment and request pages with working navbar

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@
 
 import React from 'react';
 import './App.css';
-import Header from './components/header.js';
+import Navbar from './components/navbar.js';
+import Home from './pages/home.js';
+import Request from './pages/request.js';
+import Assignments from './pages/assignments.js'
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
     return (
         <div className="App">
-            <Header/>
+            <Navbar />
+            <div className="containter">
+                <Routes>
+                    <Route path='/home' element={<Home />} />
+                    <Route path='/request' element={<Request />} />
+                    <Route path='/assignments' element={<Assignments />} />
+                </Routes>
+            </div>
         </div>
     );
 }
